6949834d1a518a42eeed5367d501ff370daaa4ff
Rob Misasi <Rob.misasi@stryker.com>
2022-05-18 10:58:18 -0400

    Fix import typo

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 
-import Header from "./components/header";
+import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
