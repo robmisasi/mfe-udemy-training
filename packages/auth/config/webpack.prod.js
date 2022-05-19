f3ae4d2966c666dc98275dade56ca296be254946
Rob Misasi <Rob.misasi@stryker.com>
2022-05-19 15:44:33 -0400

    Fix auth prod package name.

--- packages/auth/config/webpack.prod.js
+++ packages/auth/config/webpack.prod.js
@@ -11,7 +11,7 @@ const prodConfig = {
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "Auth",
+      name: "auth",
       filename: "remoteEntry.js",
       exposes: {
         "./AuthApp": "./src/bootstrap",
